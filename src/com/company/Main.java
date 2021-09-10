57b5a1a3ab0df502a8b7b6b3f836a8cf78391567
Ivan Zaets <ivan.i.zaets@gmail.com>
2021-09-10 17:35:43 +0300

    correction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         FileSearchByExtension search = new FileSearchByExtension();
-        search.findFiles(new File("D:/IdeaProjects(Save)/Homework8Task1"), ".java");
+        search.findFiles(new File("./"), ".java");
     }
 }
